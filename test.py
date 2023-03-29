5b371706eae924789f990bb59516e1bce24151cf
KartikBhatnagar <kartikbhatnagar111@gmail.com>
2023-03-29 08:08:18 +0530

    Initial commit with hello world flask program

---
+++ test.py
@@ -0,0 +1,12 @@
+print("hello")
+for i in range(1,10):
+    print(f"{30:2} * {i:2} = {90*i:<4}")
+#import pytube
+import sys
+print(sys.executable)
+import flask
+print(flask)
+
+#from youtube_transcript_api import YouTubeTranscriptApi
+
+#e
