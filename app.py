8df729f12614c211973278668e3224c0f725ab4f
KartikBhatnagar <kartikbhatnagar111@gmail.com>
2023-03-31 08:53:57 +0530

    feature add delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,9 +20,28 @@ class Todo(db.Model):
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        return("hello")
-    else :
-        return render_template("index.html ")
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
 
+        try :
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return("There was an error adding your task")
+    else :
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html ",tasks = tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was an error deleting the task"
 if __name__ == "__main__":
     app.run(debug=True)
